a00793820ee011c806942c545f8547d7491b49af
Igor Vetekha <i.vetekha@tjip.com>
2023-12-01 15:27:54 +0100

    Initial commit, including the Day1 human logic (as-is) solution

---
+++ AdventOfCode_2023_Tests/Day01/Day01.cs
@@ -0,0 +1,62 @@
+using System.Diagnostics;
+using AdventOfCode_2023_Tests;
+using System.Linq;
+
+namespace AdventOfCode_2023_Tests.Day01
+{
+    [TestClass]
+    public class Day01
+    {
+        public string currentPath => Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, GetType().Name);
+        public string InputFileName => GetType().Name + ".txt";
+        public string InputFilePath => Path.Combine(currentPath, InputFileName);
+        public string OutputFileName => GetType().Name + "_output.txt";
+        public string OutputFilePath => Path.Combine(currentPath, OutputFileName);
+
+
+        [TestInitialize()]
+        public void Preset()
+        {
+            
+        }
+
+        [TestMethod]
+        public void Puzzle_1()
+        {
+            var inputLines = File.ReadAllLines(InputFilePath);
+            var modifiedLines = new List<int>();
+
+            foreach (var line in inputLines)
+            {
+                modifiedLines.Add(Shared.GetFirstAndLastNumber(line));
+            }
+
+            var sumOfAllNumbers = modifiedLines.Sum();
+
+            var outputText = $"P1: The sum of all calibration values inside the provided document is {sumOfAllNumbers}";
+            Debug.WriteLine(outputText);
+            Shared.SendOutputToFile(OutputFilePath, outputText);
+        }
+
+        [TestMethod]
+        public void Puzzle_2()
+        {
+            var inputLines = File.ReadAllLines(InputFilePath);
+            var modifiedLines = new List<int>();
+
+            var counter = 0;
+            foreach (var line in inputLines)
+            {
+                counter++;
+                modifiedLines.Add(Shared.GetFirstAndLastNumberWithSpelledNumbers(line));
+            }
+
+            var sumOfAllNumbers = modifiedLines.Sum();
+
+            var outputText = $"P2: The sum of all calibration values inside the provided document is {sumOfAllNumbers}";
+            Debug.WriteLine(outputText);
+            Shared.SendOutputToFile(OutputFilePath, outputText);
+        }
+
+    }
+}
---
+++ AdventOfCode_2023_Tests/Shared.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode_2023_Tests
+{
+    public static class Shared
+    {
+        public static void SendOutputToFile(string path, string outputText)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine($"============Output at {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}============");
+                sw.WriteLine(outputText);
+                sw.WriteLine("============End of output============");
+            }
+        }
+
+        public static string RemoveCharactersFromString(string inputString)
+        {
+            Regex rgx = new Regex("[^0-9.]");
+            var output = rgx.Replace(inputString, "");
+            return output;
+        }
+
+        public static int GetFirstAndLastNumberFromString(string inputString)
+        {
+            //Note, ince ase of a single character, this will return the same character twice, because it will call [0] index twice (Length of 1 minus 1 = 0)
+            var firstAndLastCharacters = $"{inputString[0]}{inputString[inputString.Length - 1]}";
+            return int.Parse(firstAndLastCharacters);
+        }
+
+        public static int GetFirstAndLastNumber(string inputString)
+        {
+            return GetFirstAndLastNumberFromString(RemoveCharactersFromString(inputString));
+        }
+
+        //Note: Initially, I thought it would be sufficient to remove the found string occurances, but using same letter for 2 different words is allowed in this puzzle...
+        public static string ConvertSpelledNumbersToNumbers(string inputString)
+        {
+            var convertedString = inputString;
+
+            foreach (var keyValuePair in spelledNumbersDictionary)
+            {
+                convertedString = Regex.Replace(convertedString, keyValuePair.Value, keyValuePair.Key.ToString());
+            }
+
+            return convertedString;
+        }
+
+        public static string ConvertSpelledNumbersToNumbersWithoutRemovingText(string inputString)
+        {
+            var numbersToInsertAtPosition = new Dictionary<int, int>();
+
+            foreach (var keyValuePair in spelledNumbersDictionary)
+            {
+                var regex = new Regex(keyValuePair.Value);
+                foreach (Match match in regex.Matches(inputString))
+                {
+                    numbersToInsertAtPosition.Add(match.Index, keyValuePair.Key);
+                }
+            }
+
+            var convertedString = inputString;
+            var addedNumbersCount = 0;
+
+            foreach (var keyValuePair in numbersToInsertAtPosition)
+            {
+                convertedString = convertedString.Insert(keyValuePair.Key + addedNumbersCount, keyValuePair.Value.ToString());
+                addedNumbersCount++;
+            }
+
+            return convertedString;
+        }
+
+        public static string[] spelledNumbers = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        public static Dictionary<int, string> spelledNumbersDictionary = new Dictionary<int, string>()
+        {
+            {1, "one"},
+            {2, "two"},
+            {3, "three"},
+            {4, "four"},
+            {5, "five"},
+            {6, "six"},
+            {7, "seven"},
+            {8, "eight"},
+            {9, "nine"}
+        };
+
+        public static int GetFirstAndLastNumberWithSpelledNumbers(string inputString)
+        {
+            return GetFirstAndLastNumberFromString(RemoveCharactersFromString(ConvertSpelledNumbersToNumbersWithoutRemovingText(inputString)));
+        }
+    }
+}
